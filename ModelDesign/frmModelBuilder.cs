017ed5aa37caf9d1b2e9c0139224c63ca3d7825f
tangning <tangning@penghaisoft.com>
2023-07-10 22:12:30 +0800

    DBType to CSType

--- ModelDesign/frmModelBuilder.cs
+++ ModelDesign/frmModelBuilder.cs
@@ -25,7 +25,7 @@ namespace ModelDesign
             string DB = txtDB.Text.Trim();
             string SERVEr = txtServer.Text.Trim();
 
-             mysqlhelper = new MySqlHelp(SERVEr, UID, UPWD, DB);
+            mysqlhelper = new MySqlHelp(SERVEr, UID, UPWD, DB);
 
             if (mysqlhelper.GetConnection() == null)
             {
@@ -42,7 +42,7 @@ namespace ModelDesign
 
         public DataTable GetTables()
         {
-          
+
             //string[] restriction = new string[4];
             //restriction[1] = "dbo";
             //var conn = sqlhelper.GetConnection();     
@@ -65,7 +65,7 @@ namespace ModelDesign
         }
 
         public DataTable GetColumns()
-        { 
+        {
             var conn = mysqlhelper.GetConnection();
             string[] restriction = new string[4];
             restriction[2] = cmbTables.Text;
@@ -74,101 +74,96 @@ namespace ModelDesign
             return dt;
         }
 
+        private string DBTypeTransformToCSType(string dbType)
+        {
+            string csType = "UnknowType";
+            switch (dbType)//row[7]
+            {
+                case "text":
+                case "varchar":
+                case "nvarchar":
+                    csType = "string";
+                    break;
+                case "bit":
+                    csType = "bool";
+                    break;
+                case "tinyint":
+                    csType = "byte";
+                    break;
+                case "int":
+                    csType = "int";
+                    break;
+                case "bigint":
+                    csType = "long";
+                    break;
+                case "datetime":
+                case "date":
+                case "time":
+                case "timestamp":
+                    csType = "DateTime";
+                    break;
+                case "decimal":
+                case "money":
+                case "numeric":
+                case "smallmoney":
+                    csType = "decimal";
+                    break;
+                case "real":
+                    csType = "float";
+                    break;
+                case "float":
+                    csType = "float";
+                    break;
+                case "double":
+                    csType = "double";
+                    break;
+                case "smallint":
+                    csType = "short";
+                    break;
+                default:
+                    break;
+            }
+            return csType;
+        }
+
         public void WriteText(DataTable dt)
         {
-
             //创建类
             string uid = txtUser.Text.Trim();
             string pwd = txtPwd.Text.Trim();
             string db = txtDB.Text.Trim();
             string tableName = cmbTables.Text;
             string tbName = tableName;
-            //if (tableName.StartsWith("t_"))
-            //{
-            //    tbName = tableName.Substring(2, tableName.Length - 2);
-            //}
-            //else
-            //{
-            //    tbName = tableName;
-            //}
-            string conText = $"public class {tbName}\r\n";
-            conText += "{\r\n";
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"public class {tbName}\r\n");
+            builder.Append("{\r\n");
 
             //构造字段
-            foreach (System.Data.DataRow row in dt.Rows)
+            foreach (DataRow row in dt.Rows)
             {
-                switch (row["DATA_TYPE"].ToString())//row[7]
-                {
-                    case "text":
-                    case "varchar":
-                    case "nvarchar":
-                        conText += $"{t}private string {GetField(row["COLUMN_NAME"].ToString())};\r\n";//row[3]
-                        break;
-                    case "bit":
-                    case "tinyint":
-                    case "int":                  
-                        conText += $"{t}private int {GetField(row["COLUMN_NAME"].ToString())};\r\n";//row[3]
-                        break;
-                    case "bigint":
-                        conText += $"{t}private long {GetField(row["COLUMN_NAME"].ToString())};\r\n";//row[3]
-                        break;
-                    case "datetime":
-                        conText += $"{t}private DateTime {GetField(row["COLUMN_NAME"].ToString())};\r\n";//row[3]
-                        break;
-                    case "float":
-                    case "decimal":
-                        conText += $"{t}private float {GetField(row["COLUMN_NAME"].ToString())};\r\n";//row[3]
-                        break;
-                    default:
-                        conText += $"{t}private string {GetField(row["COLUMN_NAME"].ToString())};\r\n";//row[3]
-                        break;
-                }
+                string csType = DBTypeTransformToCSType(row["DATA_TYPE"].ToString());//row[7]
+                builder.Append($"{t}private {csType} {GetField(row["COLUMN_NAME"].ToString())};\r\n");//row[3]
             }
-
+            builder.Append("\r\n");
             //构造空的类方法
-            conText += $"{t}public {tbName}()" + " {  }\r\n";
-
+            builder.Append($"{t}public {tbName}()" + " {  }\r\n");
+            builder.Append("\r\n");
             //构造属性
-            foreach (System.Data.DataRow row in dt.Rows)
+            foreach (DataRow row in dt.Rows)
             {
-                conText += $"{t}/// <summary>\r\n";
-                conText += $"{t}/// {row["COLUMN_COMMENT"]}\r\n";
-                conText += $"{t}/// </summary>\r\n";
-                switch (row["DATA_TYPE"].ToString())//row[7]
-                {
-                    case "text":
-                    case "varchar":
-                    case "nvarchar":
-                        {
-                            conText += $"{t}public string {row["COLUMN_NAME"].ToString()}\r\n";
-                            break;
-                        }
-                    case "bit":
-                    case "tinyint":
-                    case "int":
-                    case "bigint":
-                        {
-                            conText += $"{t}public int {row["COLUMN_NAME"].ToString()}\r\n";
-                            break;
-                        }
-                    case "datetime":
-                        conText += $"{t}public DateTime {row["COLUMN_NAME"].ToString()}\r\n";
-                        break;
-                    case "float":
-                    case "decimal":
-                        conText += $"{t}public float {row["COLUMN_NAME"].ToString()}\r\n";
-                        break;
-                    default:
-                        conText += $"{t}public string {row["COLUMN_NAME"].ToString()}\r\n";
-                        break;
-                }
-                conText += (t + "{\r\n");
-                conText += (t + t + "get{ " + "return " + GetField(row["COLUMN_NAME"].ToString()) + "; }\r\n");
-                conText += (t + t + "set{ " + GetField(row["COLUMN_NAME"].ToString()) + " = value; }\r\n");
-                conText += (t + "}\r\n");
+                builder.Append($"{t}/// <summary>\r\n");
+                builder.Append($"{t}/// {row["COLUMN_COMMENT"]}\r\n");
+                builder.Append($"{t}/// </summary>\r\n");
+                string csType = DBTypeTransformToCSType(row["DATA_TYPE"].ToString());//row[7]
+                builder.Append($"{t}public {csType} {row["COLUMN_NAME"].ToString()}\r\n");
+                builder.Append(t + "{\r\n");
+                builder.Append(t + t + "get{ " + "return " + GetField(row["COLUMN_NAME"].ToString()) + "; }\r\n");
+                builder.Append(t + t + "set{ " + GetField(row["COLUMN_NAME"].ToString()) + " = value; }\r\n");
+                builder.Append(t + "}\r\n");
             }
-            conText += "}";
-            this.txtTable.Text = conText;
+            builder.Append("}");
+            this.txtTable.Text = builder.ToString();
         }
 
         private string t = "    ";
